280ded67defb6d9985d3a078b76963f19b6e9996
MartijnStraatman <mhst78@gmail.com>
2016-02-26 10:17:29 +0000

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+     public static void main (String [] args) {
+          
+          if (args.length > 0) {
+             System.out.println(args[0]);
+          } 
+
+          System.out.println("hello world");
+          System.exit(0);
+	} 
+
+
+}
